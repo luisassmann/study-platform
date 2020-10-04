f87a3ef8c5138870b7bcf58a55781c8e66350cde
luisassmann <luisassmann@users.noreply.github.com>
2020-10-04 12:50:46 -0300

    ending of base project

--- public/scripts/addField.js
+++ public/scripts/addField.js
@@ -1,11 +1,17 @@
+// Procurar o botao
 document.querySelector("#add-time")
+// Quando clicar no botao
 .addEventListener('click', cloneField)
 
+// Executar uma acao
 function cloneField() {
+  // Duplicar os campos
   var newFieldContainer = document.querySelector(".schedule-item").cloneNode(true)
+  // Limpar os campos
   const fields = newFieldContainer.querySelectorAll("input")
   fields.forEach(function(field) {
     field.value = ""
   })
+  // Colocar na page
   document.querySelector("#schedule-items").appendChild(newFieldContainer)
 }
---
+++ src/server.js
@@ -0,0 +1,26 @@
+// server
+const express = require('express')
+const server = express()
+
+const { pageLanding, pageStudy, pageGiveClasses, saveClasses, pageSuccess } = require('./pages')
+
+// configure nunjucks
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+  express: server,
+  noCache: true
+})
+
+server
+// receber os dados req.body
+.use(express.urlencoded({ extended: true }))
+// configure static files (css, js, img's)
+.use(express.static("public"))
+// routes of the web-app
+.get("/", pageLanding)
+.get("/study", pageStudy)
+.get("/give-classes", pageGiveClasses)
+.get("/success", pageSuccess)
+.post("/save-classes", saveClasses)
+
+.listen(5000)
